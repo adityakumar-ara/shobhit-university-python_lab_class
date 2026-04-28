e047e636a488212dd39e4630f2f34ad89a1acf46
Aditya Kumar <adityaara7667@gmail.com>
2026-04-28 15:52:50 +0530

    add more

--- python_class/Module/importmodule.py
+++ python_class/Module/importmodule.py
@@ -1,9 +1,10 @@
-import mymodule
+# import mymodule
 
-string = input("Enter a string: ")
+# string = input("Enter a string: ")
 
-result = mymodule.char_frequency(string)
+# result = mymodule.char_frequency(string)
 
-print("Character Frequency:")
-for key, value in result.items():
-    print(key, ":", value)
+# print("Character Frequency:")
+# for key, value in result.items():
+#     print(key, ":", value)
+
