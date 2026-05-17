255937b625ef0ce117eef9d29ed639af4f6c5921
Aditya Kumar <adityaara7667@gmail.com>
2026-05-17 11:00:06 +0530

    add more

---
+++ python_class/stirngformatting/% Operator.py
@@ -0,0 +1,8 @@
+name = "Aman"
+score = 95
+
+# % operator ka use
+message = "Student ka naam %s hai aur uske %d marks aaye hain." % (name, score)
+print(message)
+
+# Output: Student ka naam Aman hai aur uske 95 marks aaye hain.
